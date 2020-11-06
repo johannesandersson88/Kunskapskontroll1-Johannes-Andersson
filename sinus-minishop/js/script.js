596d94cb43c4284fcef938532f69adc361e362ac
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2020-11-06 13:37:13 +0100

    del 2
    bakgrundsfärg,
    knappar både vid tryck och tillbaka

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -1,6 +1,6 @@
 /* ÅTERSTÄLLNINGSKNAPP */
 
-// Här skapar vi en ny knapp för att undvika förvirring kring dom som finns. 
+// Här skapar vi en ny knapp för att undvika förvirring kring dom knappar som finns. Röd och vit för tydlighet. 
 
 // Bestämmer efter vilken section knappen ska hamna. 
 let lastProduct = document.querySelector('.art-3');
@@ -9,10 +9,10 @@ let lastProduct = document.querySelector('.art-3');
 let newButton = document.createElement('button');
 
 // Hur den kommer se ut 
-newButton.innerText = 'Reset All Javascript';
+newButton.innerText = 'Reset To Original';
 newButton.style.backgroundColor = 'red';
 newButton.style.color = 'white';
-newButton.style.padding = '20px 20px';
+newButton.style.padding = '40px 40px';
 newButton.style.fontSize = '20px';
 newButton.style.cursor = 'pointer';
 
@@ -25,9 +25,34 @@ newButton.addEventListener('click', function () {
     // Ändrar tillbaka färg på tröja 2 till orginal. 
     changeColourPhoto[3].src = "img/hoodie-fire.png";
 
+    // Ändrar tillbaka texten till orginalet i alla sektioner.  
+    paragraphText[0].innerText = 'Tryck på knappen för beskrivning av produkten.';
+    paragraphText[0].style.fontWeight = 'bold';
+    paragraphText[0].style.fontSize = '18px';
+
+    // Visar elementen igen och ger samma bakgrund på sektion 3.
+    let hideAllOne = document.querySelector('.art-3 h2')
+    let hideAllTwo = document.querySelector('.art-3 h3')
+    let hideAllThree = document.querySelector('.art-3 p')
+    hideAllOne.style.display = 'block';
+    hideAllTwo.style.display = 'block';
+    hideAllThree.style.display = 'block';
+
+    bgChange.style.background = 'white';
+
+    // Ändrar tillbaka knappen i sektion 3
+    newButtonChange[2].innerText = 'PRESS ME';
+    newButtonChange[2].style.backgroundColor = '#222';
+    newButtonChange[2].style.color = 'white';
+
+    // Knappen tillbaka i sektion 2
+    newButtonChange[1].innerText = 'PRESS ME';
+    // Knappen i Sektion 1
+    newButtonChange[0].innerText = 'PRESS ME';
+
 });
 
-// Ny text, istället för Lorem. Kan ändras via knapparna. Väljer elementen här.  
+/*  [1]  Ny text på artiklar och knappar.  */
 let paragraphText = document.querySelectorAll('p');
 
 // Första Sektionen 
@@ -43,27 +68,81 @@ paragraphText[2].innerText = 'Tryck på knappen för att se vilka färger som fi
 paragraphText[2].style.fontWeight = 'bold';
 paragraphText[2].style.fontSize = '18px';
 
+// Tillgång till alla knappar för textändring
+let newButtonChange = document.querySelectorAll('button');
+newButtonChange[0].innerText = 'PRESS ME';
+newButtonChange[1].innerText = 'PRESS ME';
+newButtonChange[2].innerText = 'PRESS ME';
 
 
 
+/* [2] Byter ut färgen på hood 2. */
 
-
-
-// 2 Byter ut ett foto. Man ska kunna trycka för att välja färg, toggle.
-
-// Vilken bild vi ska ändra, skapar ett Element och lägger in HTML koden för nya bilden. 
+// Ger tillgång till bilderna
 let changeColourPhoto = document.querySelectorAll('img');
 
-// Vilken knapp vi ska ändra bild med 
+// Ger tillgång till alla knappar
 let changeColorButton = document.querySelectorAll('button');
 
 // Funktionen när man trycker, ändras färgen på tröjan.
+
+// Vilken av knapparna som ska tryckas på 
 changeColorButton[1].addEventListener('click', function () {
+
+    // Vilken av bilderna som ska bytas ut, ändrar source koden för att lägga in ny bild. 
     changeColourPhoto[3].src = "img/hoodie-forrest.png";
+
+    // Ny text på knappen 
+    newButtonChange[1].innerText = 'Hit RESET';
 });
 
 
+/*  [3] Byter ut informationen på paragraph i första sektionen. */
 
-console.log(changeColourPhoto)
+// Ger tillgång till paragraphs. Queryselector för jag vet att den väljer första paragraph jag har i HTML. 
+let productInfo = document.querySelector('p');
+
+// Ger tillgång till knapparna
+let changeTextButton = document.querySelectorAll('button');
+
+// Funktionen när man trycker, då får man en beskrivning av produkten. 
+changeTextButton[0].addEventListener('click', function () {
+
+    // Ny stil för att visa en annan sorts text. 
+    paragraphText[0].innerText = 'Hoodtröja i förpackning av fullt återandvändbart material. Finns i alla storlekar.';
+    paragraphText[0].style.fontWeight = 'normal';
+    paragraphText[0].style.fontSize = '16px';
+
+    // text i knappen ändras
+    newButtonChange[0].innerText = 'Hit RESET';
+});
+
+/* [4] Ändra bakgrundsfärg på tredje sektionen, visar vilka färger man kan få. */
+
+// Ger tillgång till article för ändra hela bakgrundsfärgen. 
+let bgChange = document.querySelector('.art-3');
+
+// Ger tillgång till knapparna 
+let bgChangeButton = document.querySelectorAll('button');
+
+// Funktionen där du får upp nytt färgspektrum. 
+
+bgChangeButton[2].addEventListener('click', function () {
+    bgChange.style.background = 'linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)';
+
+    // Dölj text och rubriker, men inte knappen.
+    let hideAllOne = document.querySelector('.art-3 h2')
+    let hideAllTwo = document.querySelector('.art-3 h3')
+    let hideAllThree = document.querySelector('.art-3 p')
+    hideAllOne.style.display = 'none';
+    hideAllTwo.style.display = 'none';
+    hideAllThree.style.display = 'none';
+
+    newButtonChange[2].innerText = 'Hit Reset';
+    newButtonChange[2].style.backgroundColor = 'white';
+    newButtonChange[2].style.color = 'black';
+
+})
+
 
 
